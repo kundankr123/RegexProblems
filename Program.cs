88796744c2a3bf19b834961f277c7b759849f2e8
kundan <goswamikundan123@gmail.com>
2023-03-02 18:13:46 +0530

    Adding file to check the name as stated in UseCase1.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,21 @@
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Regex Problems Master Branch.");
+
+            Patternmatch pattern = new Patternmatch();
+
+            Console.WriteLine("Enter your Name: ");
+            string name = Console.ReadLine();
+            bool val = pattern.validate(name);
+
+            if (val)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name!");
+            }
         }
     }
 }
